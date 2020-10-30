cc34bc0f224a273c8ff220176e48947ea23c3214
Eric Kida <51220926+EricKida@users.noreply.github.com>
2020-10-30 18:42:16 -0300

    Adição Galeria - Perfil

---
+++ inc/galeria.php
@@ -0,0 +1,3 @@
+ <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.25/jquery.fancybox.min.css" />
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.25/jquery.fancybox.min.js"></script>
